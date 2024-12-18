927b8bed363803f73bd04769341e8851b8a13e8d
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-12-18 12:03:29 +0100

    .

---
+++ missing_number.js
@@ -0,0 +1,16 @@
+const { log } = require('console');
+var readline = require('readline');
+var r = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    terminal: false
+})
+var n
+r   .once('line', function (l1) {
+    n = Number(l1)
+    r.once('line', function (l2) {
+        a = l2.split(' ').map(Number)
+        console.log((n * (n + 1) / 2) - a.reduce((a, b) => a + b))
+        r.close()
+    })
+})  
